7bd725c046423d396e97f06acc8ac3cbd8c506a5
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-16 13:15:09 -0400

    Mastery Project Beginning

---
+++ MasteryHospital/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasteryHospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
