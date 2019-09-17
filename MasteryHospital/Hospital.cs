7fa32e3a2abda7641c5a0fa427d3dcc091e462ef
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-17 11:02:17 -0400

    StartedCodingMethodsInClassses

---
+++ MasteryHospital/Hospital.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasteryHospital
+{
+    class Hospital
+    {
+        public List<Patient> patientsInHospital = new List<Patient>();
+
+        public void AddPatients(Patient patient)
+        {
+            patientsInHospital.Add(patient);
+        }
+
+        public void ShowListOfPatients()
+        {
+            foreach (Patient patient in patientsInHospital)
+                patient.PatientInfo();
+        }
+        public void ShowStatusOfPatient()
+        {
+            foreach (Patient patient in patientsInHospital)
+            {
+                patient.PatientStatus();
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ MasteryHospital/Patient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasteryHospital
+{
+    public class Patient
+    {
+
+        public string PatientName { get; set; }
+        public int PatientBlood { get; set; }
+        public int PatientHealth { get; set; }
+
+
+        public Patient()
+        {
+            PatientBlood = 20;
+            PatientHealth = 10;
+        }
+
+        public void AddPatient()
+        {
+            Console.WriteLine("Add a new Patient");
+             PatientName = Console.ReadLine().ToLower();
+
+        }
+         public void PatientInfo()
+        {
+            Console.WriteLine(PatientName);
+        }
+
+        public void PatientStatus()
+        {
+            Console.WriteLine("Patienet blood level:" + PatientBlood);
+            Console.WriteLine("Patient health level:" + PatientHealth);
+        }
+
+    }
+}
